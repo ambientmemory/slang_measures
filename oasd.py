97e750e221f7d991123822828b571df9f1bd4200
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2016-06-26 11:22:23 -0400

    oasd but distance values 0?

--- oasd.py
+++ oasd.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
 Runs the optimal string alignment distance algorithm
 This computes the number of edit operations needed to
@@ -13,4 +14,30 @@ def osa_dist(cn_word, sl_word):
 	:param sl_word: Slang known English word
 	:return: integer distance between two strings
 	'''
-	
+	d = np.zeros((len(cn_word)+1,len(sl_word)+1))
+
+	for i in range(0, len(cn_word)):
+		d[i, 0] = i
+
+	for j in range(0, len(sl_word)):
+		d[0, j] = j
+
+	for i in range(1, len(cn_word)):
+		for j in range(1, len(sl_word)):
+			if cn_word[i] == sl_word[j]:
+				cost = 0
+			else:
+				cost = 1
+			d[i, j] = min([d[i-1, j]+1,
+			               d[i, j-1]+1,
+			               d[i-1, j-1]+cost])
+			if i>1 and j>1 and cn_word[i] == sl_word[j-1] and cn_word[i-1]==sl_word[j]:
+				d[i, j] = min([d[i,j], d[i-2, j-2]+cost])
+	return d[len(cn_word), len(sl_word)]
+
+
+sl_word = input("Please enter slang word: ")
+cn_word = input("Please enter canon word: ")
+output = osa_dist(cn_word, sl_word)
+print('Output of OASD is:'+'\n')
+print(output)
