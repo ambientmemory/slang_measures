6a189125d489414fa84d5d765d2e04c0acbcb5b3
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2016-06-26 11:31:20 -0400

    oasd works beautifully!

--- oasd.py
+++ oasd.py
@@ -1,4 +1,6 @@
 import numpy as np
+global debug;
+debug= True
 '''
 Runs the optimal string alignment distance algorithm
 This computes the number of edit operations needed to
@@ -14,7 +16,7 @@ def osa_dist(cn_word, sl_word):
 	:param sl_word: Slang known English word
 	:return: integer distance between two strings
 	'''
-	d = np.zeros((len(cn_word)+1,len(sl_word)+1))
+	d = np.zeros((len(cn_word),len(sl_word)))
 
 	for i in range(0, len(cn_word)):
 		d[i, 0] = i
@@ -28,12 +30,20 @@ def osa_dist(cn_word, sl_word):
 				cost = 0
 			else:
 				cost = 1
-			d[i, j] = min([d[i-1, j]+1,
-			               d[i, j-1]+1,
-			               d[i-1, j-1]+cost])
-			if i>1 and j>1 and cn_word[i] == sl_word[j-1] and cn_word[i-1]==sl_word[j]:
+			d[i, j] = min([d[i-1, j]+1, #deletion
+			               d[i, j-1]+1, #insertion
+			               d[i-1, j-1]+cost]) #substitution
+			if i>1 and j>1 and \
+					cn_word[i] == sl_word[j-1] and \
+					cn_word[i-1]==sl_word[j]:
 				d[i, j] = min([d[i,j], d[i-2, j-2]+cost])
-	return d[len(cn_word), len(sl_word)]
+
+			if debug:
+				print("Debug: i = ",i,"\n")
+				print("Debug: j = ",j,"\n")
+				print("Debug: d = \n")
+				print(d)
+	return d[len(cn_word)-1, len(sl_word)-1]
 
 
 sl_word = input("Please enter slang word: ")
